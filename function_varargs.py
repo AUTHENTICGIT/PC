63656940bf656d3637a00746346e9d00217321f1
AU <leogwork@gmail.com>
2017-04-21 17:53:28 +0800

    Function·Keyword Arguments

--- function_varargs.py
+++ function_varargs.py
@@ -1,45 +1,19 @@
-# 函数
-def say_hello():
-    print('Hello world')
-say_hello()
-say_hello()
-# 函数参数
-def maxium(a, b):
-    if a < b:
-        print(b)
-    elif a == b:
-        print(a, 'is equal to', b)
-    else:
-        print(a)
-maxium(1, 3)
-maxium(3, 3)
-maxium(1, 5)
-#局部变量
-def func(x):
-    print(x)
-    x = 1111
-    print('x is changes to', x)
-x = 5
-func(x)
-print('real x is', x)
-#全局变量
-def func1():
-    global x
-    x *= 0
-    print(x)
-x = 5
-func1()
-#默认参数值
-def func2(x, y=18):
-    print('name:',x, '\t' 'age:', y)
-func2('Apple')
-func2('Bella', 21)
-func2('Cindy', 16)
-#关键字参数
-def func3(a, b=5, c=10):
-    print('a is', a, 'b is', b, 'c is', c)
-func3(2, 1, c=3)
-func3(2, 1)
-func3(c=11, a=7)
-#可变参数
 def func(a, b, *c, **d):
+    print(a)
+    print(b)
+    for x in c:
+        print(x)
+    for k,v in d.items():
+        print(k, v)
+func('a', 10, 'single_item 1', 'single_item 2', 'single_item 3', Inge=1560, John=2231, Jack=1123)
+
+#案例
+def total(a=5, *numbers, **phonebook):
+    print('a', a)
+    #遍历元祖中的所有项目
+    for single_item in numbers:
+        print('single_item', single_item)
+    for first_part,  second_part in phonebook.items():
+        print(first_part, second_part)
+
+print(total(10, 1, 2, 3, Jack=1123, John=2231, Inge=1560))
