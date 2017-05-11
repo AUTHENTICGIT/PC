9d6a3e03531fd698e2f52642d899c67da06f2218
AU <leogwork@gmail.com>
2017-05-11 17:44:55 +0800

    Exceptions

---
+++ exceptions_handle.py
@@ -0,0 +1,19 @@
+# file = input('Enter file name:')
+# try:
+#     fhand = open(file)
+# except:
+#     print('File not exist!')
+# for line in fhand:
+#     print(line, end = '')
+
+# 案例
+try:
+    # 一般会把通常的语句放在try代码块中
+    text = input('Enter something --->')
+except EOFError:
+    # 把错误处理器代码放在except代码块中
+    print('Why did you do an EOF on me?')
+except KeyboardInterrupt:
+    print('You cancelled the operation.')
+else:
+    print('You entered {}'.format(text))
