b7e758f9e306878e7b1879bb56ef4e320227e6a4
AU <leogwork@gmail.com>
2017-05-12 16:46:45 +0800

    Exceptions

--- exceptions_raise.py
+++ exceptions_raise.py
@@ -1,15 +1,33 @@
+# class ShortInputException(Exception):
+#     def __init__(self, length, atleast):
+#         Exception.__init__(self)
+#         self.length = length
+#         self.atleast = atleast
+#
+#     def is_error(self):
+#         if self.length < self.atleast:
+#             print('ShortInputException: The input was {} long, expected at least {}'.format(self.length, self.atleast))
+#         else:
+#             print('No exception was raised.')
+#
+# something = input('Enter something --> ')
+# s = ShortInputException(len(something), 3)
+# s.is_error()
+
+# 案例
 class ShortInputException(Exception):
     def __init__(self, length, atleast):
-        self.__init__(self)
+        Exception.__init__(self)
         self.length = length
         self.atleast = atleast
 
-    def is_erro(self):
-        if self.length > self.atleast:
-            print('ShortInputException: The input was {} long, expected at least {}'.format(self.length, self.atleast))
-        else:
-            print('No exception was raised.')
-
-something = input()
-
-s = ShortInputException()
+try:
+    text = input('Enter something --> ')
+    if len(text) < 3:
+        raise ShortInputException(len(text), 3)
+except EOFError:
+    print('Why did you do an EOF on me?')
+except ShortInputException as ex:
+    print('ShortInputException: The input was {0} long, expected at least {1}'.format(ex.length, ex.atleast))
+else:
+    print('No exception was raised.')
