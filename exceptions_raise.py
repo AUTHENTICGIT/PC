26488af74f24bdd3e3b2668f70f2b1ff4e2f2c0c
AU <leogwork@gmail.com>
2017-05-17 11:02:24 +0800

    Exceptions

--- exceptions_raise.py
+++ exceptions_raise.py
@@ -25,9 +25,11 @@ try:
     text = input('Enter something --> ')
     if len(text) < 3:
         raise ShortInputException(len(text), 3)
-except EOFError:    # EOFError意味着它发现了一个不期望的文件尾（文件尾是由Ctrl-d引起的）
+except EOFError:
+# EOFError意味着它发现了一个不期望的文件尾（文件尾是由Ctrl-d引起的）
     print('Why did you do an EOF on me?')
 except ShortInputException as ex:
+# 将错误类存储as（为）相应的错误名或异常名。类似于函数调用的形参（ShortInputException）和实参（ex）
     print('ShortInputException: The input was {0} long, expected at least {1}'.format(ex.length, ex.atleast))
 else:
     print('No exception was raised.')
