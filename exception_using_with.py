27e3bd92173a6d5b4083d801c25dde202d71581d
AU <leogwork@gmail.com>
2017-05-24 10:36:58 +0800

    Exceptions

--- exception_using_with.py
+++ exception_using_with.py
@@ -7,7 +7,18 @@ with open("C:/Users/xly/Desktop\poem.txt") as f:
     for line in f:
         print(line, end='')
 
-fhand = open("C:/Users/xly/Desktop\poem.txt")
-fhand.close()
-for line in fhand:
-    print(line)
+# fhand = open("C:/Users/xly/Desktop\poem.txt")
+# fhand.close()
+# for line in fhand:
+#     print(line)
+
+class Sample:
+    def __enter__(self):
+        print("In __enter__()")
+        return "Foo"
+    def __exit__(self, type, value, trace):
+        print('In __exit__()')
+def get_sample():
+    return Sample()
+with get_sample() as sample:
+    print("sample:", sample)
