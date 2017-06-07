42af55e4f3902391b9df0af62c8f41e1292c013b
AU <leogwork@gmail.com>
2017-06-07 17:05:10 +0800

    Standard Library·logging module

--- stdlib_logging.py
+++ stdlib_logging.py
@@ -1,12 +1,11 @@
-import platform
 import os
+import platform
 import logging
 
-if platform.platform().startswith('Windows'):   # platform.platform()获取操作系统名称及版本号
-    logging_file = os.path.join(os.getenv('HOMEDRIVE'), # os.getenv()读取环境变量
+if platform.platform().startswith('Windows'):
+    logging_file = os.path.join(os.getenv('HOMEDRIVE'),
                                 os.getenv('HOMEPATH'),
                                 'test.log')
-    print(os.getenv('HOMEDRIVE'), os.getenv('HOMEPATH'))
 else:
     logging_file = os.path.join(os.getenv('HOME'),
                                 'test.log')
@@ -14,19 +13,12 @@ else:
 print('Logging to', logging_file)
 
 logging.basicConfig(
-    level = logging.DEBUG,
-    format = '%(asctime)s : %(levelname)s : %(message)s',
-    filename = logging_file,
-    filemode = 'w',
+    level=logging.DEBUG,
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    filename=logging_file,
+    filemode='w',
 )
 
-logging.debug('Start of the program')
+logging.debug('Starts of the program')
 logging.info('Doing something')
 logging.warning('Dying now')
-
-import os
-
-print(os.getenv('HOMEDRIVE'))
-print(os.getenv('USERNAME'))
-print(os.getenv('HOMEPATH'))
-print(os.getenv('PYTHONPATH'))
