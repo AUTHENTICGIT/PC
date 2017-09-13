3aa0257d98af7cd2c63d32c657f7d9ecf3afb436
AUTHENTICGIT <leogwork@gmail.com>
2017-09-13 17:51:32 +0800

    update

--- What Next/practice.py
+++ What Next/practice.py
@@ -35,18 +35,24 @@ import pymysql
 
 conn = pymysql.Connect(host='localhost', user='root', passwd='', db='wds', charset='utf8')
 cur = conn.cursor()
-sql = "INSERT INTO userlist values('%s', '%s', '%s', '%s', '%s')"
-userlist = ['1442771', 37867.77, '酱油', 'https:\\/\\/u.moimg.net\\/ico\\/1442771_1499254858.jpg', 'https:\\/\\/s.moimg.net\\/new_wds\\/images\\/default-avatar.png']
-data = tuple(userlist)
-print(data)
-cur.execute(sql % data)
+sql = 'INSERT INTO userlist values(%s, %s, %s, %s, %s)'
+
+# 单行插入
+# userlist = ['1442771', 37867.77, '酱油', 'https:\\/\\/u.moimg.net\\/ico\\/1442771_1499254858.jpg', 'https:\\/\\/s.moimg.net\\/new_wds\\/images\\/default-avatar.png']
+# data = tuple(userlist)
+# print(data)
+# cur.execute(sql % data)
+
+# 批量插入
+table = [
+    ('1442771', 37867.77, '红绳会家酱油的', 'https:\\/\\/u.moimg.net\\/ico\\/1442771_1499254858.jpg', 'https:\\/\\/s.moimg.net\\/new_wds\\/images\\/default-avatar.png'),
+    ('1137032', 23107.5, '恩兔恩兔Tanyc', 'https:\\/\\/p.moimg.net\\/ico\\/1137032_20170503_1493802333_4868.jpg', 'https:\\/\\/s.moimg.net\\/new_wds\\/images\\/default-avatar.png'),
+    ('1104581', 18435.54, '一只喜欢赵粤的猫', 'https:\\/\\/u.moimg.net\\/ico\\/1104581_1504268125.jpg', 'https:\\/\\/s.moimg.net\\/new_wds\\/images\\/default-avatar.png'),
+]
+
+cur.executemany(sql, table)
+
 conn.commit()
 cur.close()
 conn.close()
 
-dic = {'user_id': '1442771', 'total_back_amount': 37867.77, 'nickname': '红绳会家酱油的', 'icon': 'https:\\/\\/u.moimg.net\\/ico\\/1442771_1499254858.jpg', 'icon_default': 'https:\\/\\/s.moimg.net\\/new_wds\\/images\\/default-avatar.png'}
-print(dic.values())
-print(type(dic.values()))
-li = list(dic.values())
-print(li)
-print(type(li))
