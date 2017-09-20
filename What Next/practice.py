8afa6fa25433351851b0896368e9a787d8c53700
AUTHENTICGIT <leogwork@gmail.com>
2017-09-20 16:09:28 +0800

    Wds

--- What Next/practice.py
+++ What Next/practice.py
@@ -28,11 +28,11 @@ import urllib.request
 # html=driver.page_source
 # print(html)
 
-# html = 'https://wds.modian.com/show_weidashang_pro/6195#1'
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-# req = urllib.request.Request(url=html, headers=headers)
-# html_data = urllib.request.urlopen(req).read().decode('utf8')
-# print(html_data)
+html = 'https://wds.modian.com/show_weidashang_pro/6195#1'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+req = urllib.request.Request(url=html, headers=headers)
+html_data = urllib.request.urlopen(req).read().decode('utf8')
+print(html_data)
 
 from matplotlib import pyplot
 
