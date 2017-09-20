9e344835ac25e799dad5e25296c2f611e9ef6e4b
AUTHENTICGIT <leogwork@gmail.com>
2017-09-20 16:24:40 +0800

    Wds

--- What Next/graph_wds.py
+++ What Next/graph_wds.py
@@ -1,13 +1,8 @@
-# import matplotlib.pyplot as plt
-# data = [1.5]*7 + [2.5]*2 + [3.5]*8 + [4.5]*3 + [5.5]*1 + [6.5]*8
-# plt.hist(data, bins=6)
-# plt.show()
-
 import pymysql
 import re
-from numpy import mean, median
 from collections import Counter
 from matplotlib import pyplot
+import time
 
 class Math:
     # 求极差
@@ -26,7 +21,7 @@ def get_median(data):
     half = len(data) // 2
     return (data[half] + data[~half]) / 2
 
-# 众数
+# 求众数
 def get_mode(arr):
     mode = []
     arr_appear = dict((a, arr.count(a)) for a in arr)  # 统计各个元素出现的次数
@@ -38,28 +33,6 @@ def get_mode(arr):
                 mode.append(k)
     return mode
 
-# table = input('请输入要计算的数据库表：')
-conn = pymysql.Connect(host='localhost',
-                       user='root',
-                       passwd='1234',
-                       db='wds',
-                       charset='utf8mb4')
-cur = conn.cursor()
-# cur.execute("SELECT total_back_number from " + table)
-cur.execute("SELECT total_back_number from userlist")
-rows = cur.fetchall()
-arr = []
-for r in rows:
-    item = float(re.findall(r".+'(.*)'.+", str(r))[0])
-    arr.append(item)
-# print(sorted(arr))
-# print(len(arr))
-
-print("极差为：{0:.2f}".format(Math.range(arr)))
-print("平均数为：{0:.2f}".format(Math.avg(arr)))
-print('中位数为：{0:.2f}'.format(get_median(arr)))
-print('众数为：', get_mode(arr))
-
 # 绘制散点图
 def drawScatter(x, y):
     # 创建散点图
@@ -70,12 +43,55 @@ def drawScatter(x, y):
     pyplot.ylabel('People')
     pyplot.title('Money & People Of ZY Wds')
 
-# 统计每个金额重复出现的次数
-count_times = dict(Counter(arr))
-for k, v in count_times.items():
-    if not k > 40000.00:
-        money = k
-        people = v
-        drawScatter(money, people)
+def main():
+    try:
+        # 一般会把通常的语句放在try代码块中
+        table = input('请输入要计算的数据库表：')
+    except EOFError:
+        # 把错误处理器代码放在except代码块中
+        print('Why did you do an EOF on me?')
+    except KeyboardInterrupt:
+        print('You cancelled the operation.')
+    else:
+        # 将在没有异常发生时执行
+        if table == 'userlist':
+            print('\n切换到zy数据库表进行查询\n')
+        elif table == 'mh':
+            print('\n切换到mh数据库表进行查询\n')
+        elif table == 'zyf':
+            print('\n切换到zyf数据库表进行查询\n')
+        else:
+            print('\n数据库不存在\n')
 
-pyplot.show()
+    conn = pymysql.Connect(host='localhost',
+                           user='root',
+                           passwd='1234',
+                           db='wds',
+                           charset='utf8mb4')
+    cur = conn.cursor()
+    cur.execute("SELECT total_back_number from " + table)
+    rows = cur.fetchall()
+    arr = []
+    for r in rows:
+        item = float(re.findall(r".+'(.*)'.+", str(r))[0])
+        arr.append(item)
+
+    print("极差为：{0:.2f}".format(Math.range(arr)))
+    print("平均数为：{0:.2f}".format(Math.avg(arr)))
+    print('中位数为：{0:.2f}'.format(get_median(arr)))
+    print('众数为：', get_mode(arr))
+    print('\n')
+
+    # 统计每个金额重复出现的次数
+    count_times = dict(Counter(arr))
+    for k, v in count_times.items():
+        if not k > 40000.00:
+            money = k
+            people = v
+            drawScatter(money, people)
+    print('散点图正在绘制中...稍后呈现...')
+    time.sleep(1)
+    pyplot.show()
+
+if __name__ == '__main__':
+    main()
