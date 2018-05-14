6f1e6084e2a660abbf003e9975831c6476198e49
AUTHENTICGIT <leogwork@gmail.com>
2018-05-14 16:44:15 +0800

    Add proxies

--- What Next/asgn_damai.py
+++ What Next/asgn_damai.py
@@ -7,6 +7,8 @@ Created on Sat May 12 18:45:46 2018
 import requests
 import time
 import itchat
+import os
+import random
 
 def search(FromData):
     searchCount = 0
@@ -17,10 +19,11 @@ def search(FromData):
     # 获取微信名
     users = itchat.search_friends('Admin')
     wxname = (users[0]['UserName'])
+    proxys = proxyCheck()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 
     while(True):
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-        r = requests.post('https://search.damai.cn/searchajax.html', data=FromData, headers=headers)
+        r = requests.post('https://search.damai.cn/searchajax.html', data=FromData, headers=headers, proxies=random.choice(proxys))
         print(r)
         result = r.text
         result= eval(result)   # 把str类型，转换为字典类型
@@ -33,9 +36,28 @@ def search(FromData):
             break
         else:
             searchCount += 1
-
             print(searchCount, time.asctime(time.localtime(time.time())))
             time.sleep(30)
+
+def proxyCheck():
+    # 代理检验是否可用
+    os.chdir(r'C:\\Users\\XLY_LR\\Desktop\\proxy')
+    url = 'https://www.baidu.com'
+    fp = open('host.txt', 'r')
+    ips = fp.readlines()
+    proxys = list()
+    for p in ips:
+        ip = p.strip('\n').split('\t')
+        proxy = 'http:\\' + ip[0] + ':' + ip[1]
+        proxies = {'proxy': proxy}
+        proxys.append(proxies)
+    for pro in proxys:
+        try:
+            s = requests.get(url, proxies=pro)
+            print(s)
+        except Exception as e:
+            print(e)
+    return proxys
 
 def main():
     data = {
